2b9209454252f0dbdaab3623dca9591efe1d4f7f
Sonam Thakur <sonamthakur1546@gmail.com>
2025-04-16 15:01:39 +0530

    react

---
+++ Day6/To Do/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Login from"./pages/Login";
+import Register from"./pages/Register";
+import Todo from"./pages/Todo";
+function App(){
+  return (
+      <>
+      
+      <h1>App</h1>
+      <Login/>
+      <Register/>
+      <Todo/>
+      </>
+  );
+}
+export default App;
